add8d5dc2a3f2148843bc7d7e63655a13e82e7a9
Jacky Zhang <jacky01.zhang@outlook.com>
2024-09-11 13:19:54 +0800

    add candidates of week 20240909

--- RandomSelection.py
+++ RandomSelection.py
@@ -5,6 +5,29 @@ def random_selection(items):
     return selected_item
 
 # Example usage
-my_list = [1, 2, 3, 4, 5]
+my_list = [
+    "hi", 
+    "J",
+    "同花顺",
+    "Codfish",
+    "蓝胖子",
+    "Yuichi",
+    "run",
+    "jayz3",
+    "笔尖微凉",
+    "阿童木",
+    "侃豺小哥",
+    "mil",
+    "2kod",
+    "灵虚",
+    "李明",
+    "oldfo4",
+    "knative",
+    "Hello",
+    "Risk off",
+    "曦月",
+    "NiGO",
+    "Gale",
+    "请务必优秀"]
 selected = random_selection(my_list)
-print("Selected item:", selected)
+print("被选中:", selected)
